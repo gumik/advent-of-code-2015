5e9aaae6134e3c33ee48899e35f553f3fdb04a29
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2022-08-30 11:24:04 +0200

    day01

--- src/Day01.hs
+++ src/Day01.hs
@@ -2,6 +2,17 @@ module Day01 ( solution ) where
 
 import Common (Solution(Solution), NoSolution(..), readNum)
 
-solution = Solution "day01" "" run
+solution = Solution "day01" "Not Quite Lisp" run
 
-run _ = (NoSolution, NoSolution)
+run input = (part1 input, part2 input)
+
+part1 :: String -> Int
+part1 = sum . mapToInts
+
+part2 :: String -> Int
+part2 = length . takeWhile (>= 0) . scanl (+) 0 . mapToInts
+
+mapToInts :: String -> [Int]
+mapToInts = map parenToInt where
+    parenToInt '(' = 1
+    parenToInt ')' = -1
