49305ed6ae006b504d67ff31f554d198bda88bb7
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2022-08-31 15:59:15 +0200

    day04

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,7 +1,26 @@
 module Day04 ( solution ) where
 
 import Common (Solution(Solution), NoSolution(..), readNum)
+import Data.ByteString.Lazy.Char8 as BS (pack)
+import Data.Digest.Pure.MD5 (md5)
+import Control.Arrow (second)
 
-solution = Solution "day04" "" run
+solution = Solution "day04" "The Ideal Stocking Stuffer" run
 
-run _ = (NoSolution, NoSolution)
+run input = (part1 hashes, part2 hashes) where
+    hashes = getHashes $ inputs input
+
+part1 :: [(Int, String)] -> Int
+part1 = findHashIdxWithLeadingZeros 5
+
+part2 :: [(Int, String)] -> Int
+part2 = findHashIdxWithLeadingZeros 6
+
+inputs :: String -> [(Int, String)]
+inputs input = map (\i -> (i, input ++ show i)) [1..]
+
+getHashes :: [(Int, String)] -> [(Int, String)]
+getHashes = map (second (show . md5 . pack))
+
+findHashIdxWithLeadingZeros :: Int -> [(Int, String)] -> Int
+findHashIdxWithLeadingZeros n = fst . head . dropWhile (any (/= '0') . take n . snd)
