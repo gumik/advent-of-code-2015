4bc7963d8315d52ef8786156041a151d254224af
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2023-12-05 21:06:47 +0000

    day10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,7 +1,16 @@
 module Day10 ( solution ) where
 
 import Common (Solution(Solution), NoSolution(..), readNum)
+import Data.List (iterate')
 
-solution = Solution "day10" "" run
+solution = Solution "day10" "Elves Look, Elves Say" run
 
-run _ = (NoSolution, NoSolution)
+run input = (length (elements !! 40), length (elements !! 50)) where
+    firstLine = head $ lines input
+    elements = iterate' lookAndSay firstLine
+
+lookAndSay :: String -> String
+lookAndSay [] = []
+lookAndSay s = show (length x) ++ first : lookAndSay xs where
+    (x, xs) = span (== first) s
+    first = head s
--- test/InputTest.hs
+++ test/InputTest.hs
@@ -42,7 +42,7 @@ tests = TestLabel "InputTest" $ test
     , inputTest Day07.solution (46065, 14134)
     , inputTest Day08.solution (1333, 2046)
     , inputTest Day09.solution (251, 898)
-    , inputTest Day10.solution (NoSolution, NoSolution)
+    , inputTest Day10.solution (360154, 5103798)
     , inputTest Day11.solution (NoSolution, NoSolution)
     , inputTest Day12.solution (NoSolution, NoSolution)
     , inputTest Day13.solution (NoSolution, NoSolution)
