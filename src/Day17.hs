e838052a3f8bb71358e312cabbd7aa0c3dfc8ba4
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2023-12-12 06:30:53 +0000

    day17 and day18

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,7 +1,35 @@
-module Day17 ( solution ) where
+module Day17 ( solution, combinations ) where
 
 import Common (Solution(Solution), NoSolution(..), readNum)
+import Data.List (tails, sort, sortBy)
+import Control.Arrow (Arrow(first))
+import Data.Ord (comparing, Down (Down))
+import Debug.Trace (traceShow, trace)
 
 solution = Solution "day17" "" run
 
-run _ = (NoSolution, NoSolution)
+run = solve . parse
+
+parse :: String -> [Int]
+parse = sortBy (comparing Down) . map readNum . lines
+
+solve :: [Int] -> (Int, Int)
+solve nums = (length combs, length shortest) where
+    combs = combinations 150 nums
+    minLength = minimum $ map length combs
+    shortest = filter ((== minLength) . length) combs
+
+-- Version to count only
+-- combinations :: Int -> [Int] -> Int
+-- combinations n nums
+--     | n < 0          = 0
+--     | n == 0         = 1
+--     | otherwise      = sum $ map (\(i, rest) -> combinations (n-i) rest) splits where
+--         splits = [first last $ splitAt i nums | i <- [1..length nums]]
+
+combinations :: Int -> [Int] -> [[Int]]
+combinations n nums
+    | n < 0          = []
+    | n == 0         = [[]]
+    | otherwise      = concatMap (\(i, rest) -> map (i:) (combinations (n-i) rest)) splits where
+        splits = [first last $ splitAt i nums | i <- [1..length nums]]
--- src/Day18.hs
+++ src/Day18.hs
@@ -1,7 +1,48 @@
 module Day18 ( solution ) where
 
-import Common (Solution(Solution), NoSolution(..), readNum)
+import Common (Solution(Solution), NoSolution(..), readNum, parseArray, inArrayBounds)
+import qualified Data.Map.Strict as M
+import Data.Array (Array, assocs, (//), (!), elems, bounds)
 
 solution = Solution "day18" "" run
 
-run _ = (NoSolution, NoSolution)
+run input = (part1 lights, part2 lights) where
+    lights = parse input
+
+data Light = On | Off deriving (Eq)
+type Lights = Array (Int, Int) Light
+
+instance Show Light where
+    show On  = "#"
+    show Off = "."
+
+parse :: String -> Lights
+parse = parseArray readChar where
+    readChar x = case x of
+        '#' -> On
+        _   -> Off
+
+part1 :: Lights -> Int
+part1 = turnedOn . (!! 100) . iterate animate
+
+part2 = turnedOn . addCorners . (!! 100) . iterate (animate . addCorners) where
+    addCorners l = l // [ ((y, x), On) | y <- [y0, ym], x <- [x0, xm] ] where
+        ((y0, x0), (ym, xm)) = bounds l
+
+turnedOn :: Lights -> Int
+turnedOn = length . filter (== On) . elems
+
+animate :: Lights -> Lights
+animate lights = lights // map animateOne (assocs lights) where
+
+    animateOne :: ((Int, Int), Light) -> ((Int, Int), Light)
+    animateOne ((y, x), light) = ((y, x), light') where
+        light' = case light of
+            On -> if neighboursOn == 2 || neighboursOn == 3 then On else Off
+            Off -> if neighboursOn == 3 then On else Off
+        neighboursOn = length $ filter (== On) $ neighbours (y, x)
+
+    neighbours :: (Int, Int) -> [Light]
+    neighbours (y, x) = [lights ! (y', x') | y' <- [y-1..y+1], x' <- [x-1..x+1],
+                                             (y', x') /= (y, x),
+                                             inArrayBounds lights (y', x')]
